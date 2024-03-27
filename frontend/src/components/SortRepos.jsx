7429c4933bb6be0ebe4a77e8c38d7aa6810f7714
khushaldhola <khushaldhola9@gmail.com>
2024-03-27 16:25:20 +0530

    homePG, explorePG, gitAPI

--- frontend/src/components/SortRepos.jsx
+++ frontend/src/components/SortRepos.jsx
@@ -1,26 +1,61 @@
-const SortRepos = () => {
+const SortRepos = ({ onSort, sortType }) => {
+	const BUTTONS = [
+		{ type: "recent", text: "Most Recent" },
+		{ type: "stars", text: "Most Stars" },
+		{ type: "forks", text: "Most Forks" },
+	];
+
 	return (
 		<div className='mb-2 flex justify-center lg:justify-end'>
-			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
-			>
-				Most Recent
-			</button>
-			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
-			>
-				Most Stars
-			</button>
-			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
-			>
-				Most Forks
-			</button>
+			{BUTTONS.map((button) => (
+				<button
+					key={button.type}
+					type='button'
+					className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${
+						button.type == sortType ? "border-blue-500" : ""
+					}`}
+					onClick={() => onSort(button.type)}
+				>
+					{button.text}
+				</button>
+			))}
 		</div>
 	);
 };
+export default SortRepos;
 
-export default SortRepos
+// const SortRepos = ({onSort, sortType}) => {
+// 	return (
+// 		<div className='mb-2 flex justify-center lg:justify-end'>
+// 			<button
+// 				type='button'
+// 				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+// 					${sortType === "recent" ? "border-red-500": ""}
+// 				`}
+// 				onClick={() => onSort("recent")}
+// 				>
+// 				Most Recent
+// 			</button>
+// 			<button
+// 				type='button'
+// 				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+// 					${sortType === "recent" ? "border-red-500": ""}
+// 				`}
+// 				onClick={() => onSort("stars")}
+// 				>
+// 				Most Stars
+// 			</button>
+// 			<button
+// 				type='button'
+// 				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+// 					${sortType === "recent" ? "border-red-500": ""}
+// 				`}
+// 				onClick={() => onSort("forks")}
+// 			>
+// 				Most Forks
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// export default SortRepos
--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,18 +1,89 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
+import Repos from "../components/Repos";
+
 const ExplorePage = () => {
+	
+	const [loading, setLoading] = useState(false);
+	const [repos, setRepos] = useState([]);
+	const [selectedLanguage, setSelectedLanguage] = useState("");
+
+	const exploreRepos = async (language) => {
+		setLoading(true);
+		setRepos([]);
+		try {
+			const res = await fetch(`https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=10`, {
+				headers:{
+					authorization: `token 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`
+				}
+			});
+			// console.log("res: ", res)
+			const data = await res.json();
+			// console.log("data: ", data)
+			setRepos(data.items);
+			// console.log("repos.items: ", repos.items)
+
+			setSelectedLanguage(language);
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+	
 	return (
 		<div className='px-4'>
 			<div className='bg-red-glass max-w-2xl mx-auto rounded-md p-4'>
 				<h1 className='text-xl font-bold text-center'>Explore Popular Repositories</h1>
 				<div className='flex flex-wrap gap-2 my-2 justify-center'>
-					<img src='/javascript.svg' alt='JavaScript' className='h-11 sm:h-20 cursor-pointer duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' />
-					<img src='/typescript.svg' alt='TypeScript logo' className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' />
-					<img src='/c++.svg' alt='C++ logo' className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:brightness-125 hover:border-white hover:border-b-2 rounded-md' />
-					<img src='/python.svg' alt='Python logo' className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' />
-					<img src='/java.svg' alt='Java logo' className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' />
+					<img 
+						src='/javascript.svg' 
+						alt='JavaScript' 
+						className='h-11 sm:h-20 cursor-pointer duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' 
+						onClick={() => exploreRepos("javascript")}
+					/>
+					<img 
+						src='/typescript.svg' 
+						alt='TypeScript logo' 
+						className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' 
+						onClick={() => exploreRepos("typescript")}
+					/>
+					<img 
+						src='/c++.svg' 
+						alt='C++ logo' 
+						className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:brightness-125 hover:border-white hover:border-b-2 rounded-md' 
+						onClick={() => exploreRepos("c++")}
+					/>
+					<img 
+						src='/python.svg' 
+						alt='Python logo' 
+						className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' 
+						onClick={() => exploreRepos("python")}
+					/>
+					<img 
+						src='/java.svg' 
+						alt='Java logo' 
+						className='h-11 sm:h-20 cursor-pointer transition-all duration-100 ease-in-out hover:border-white hover:border-b-2 rounded-md' 
+						onClick={() => exploreRepos("java")}
+					/>
 				</div>
+
+				{repos.length > 0 && (
+					<h2 className='text-lg font-semibold text-center my-4'>
+						<span className='bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full '>
+							{selectedLanguage.toUpperCase()}{" "}
+						</span>
+						Repositories
+					</h2>
+				)}
+				{!loading && repos.length > 0 && <Repos repos={repos} alwaysFullWidth/>}
+				{loading && <Spinner />}
+
 			</div>
 		</div>
 	);
 };
 
 export default ExplorePage
+// https://api.github.com/search/repositories?q=language:${_lang_}&sort=stars&order=desc&per_page=10
